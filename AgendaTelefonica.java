76e5c52eace206f48ca664cce8be14710de4cec8
miguel <miganrobles@gmail.com>
2016-03-04 12:53:37 +0100

    Creada clase AgendaTelefonica con los métodos enterNumber y lookupNumber

---
+++ AgendaTelefonica.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+/**
+ * Write a description of class AgendaTelefonica here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AgendaTelefonica
+{
+    // instance variables - replace the example below with your own
+    private HashMap<String, String> agendaTelefonos;
+
+    /**
+     * Constructor for objects of class AgendaTelefonica
+     */
+    public AgendaTelefonica()
+    {
+        agendaTelefonos = new HashMap<>();
+    }
+
+    /**
+     * Permite almacenar un nuevo número en la agenda indicando el nombre del contacto
+     * y su numero de telefono asociado
+     */
+    public void enterNumber(String name, String number) 
+    {
+        agendaTelefonos.put(name, number);
+    }
+
+    /**
+     * Devuelve el numero de teléfono del contacto indicado. Si el contacto no existe, 
+     * devuelve null.
+     */
+    public String lookupNumber(String name)
+    {
+        return agendaTelefonos.get(name);
+    }
+}
